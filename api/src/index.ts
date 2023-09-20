ce8f6743d09ae588e4a25614ccb0dcd2dc48da0b
adriansajjan <adriansajjan2001@gmail.com>
2023-09-20 19:31:58 +0530

    Minor changes

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,10 +2,6 @@ import { Server } from "@/package/server";
 import { WebSocket } from "@/package/socket";
 
 const server = Server.create()
-  .middleware({
-    isGlobal: true,
-    middleware: (req, res) => {},
-  })
   .handler({
     route: "/",
     method: "GET",
@@ -15,19 +11,6 @@ const server = Server.create()
           url: req.url,
           method: req.method,
           message: "Server is up and running",
-        })
-      );
-    },
-  })
-  .handler({
-    route: "/api",
-    method: "GET",
-    handler: (req, res) => {
-      res.writeHead(200, { "Content-Type": "application/json" }).end(
-        JSON.stringify({
-          url: req.url,
-          method: req.method,
-          message: "API is up and running",
         })
       );
     },
